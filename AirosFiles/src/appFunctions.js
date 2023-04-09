d0bead537328e9c40f604ca91443f93608aad3d8
MigueldeHaroce <migueldeharoce@gmail.com>
2023-04-09 15:00:14 +0000

    Apr 9, 2023, 5:00 PM

--- AirosFiles/src/appFunctions.js
+++ AirosFiles/src/appFunctions.js
@@ -2,15 +2,11 @@ const closeBtn = document.getElementById("closeBtn");
 const minimizeBtn = document.getElementById("minimizeBtn");
 const restoreBtn = document.getElementById("restoreBtn");
 
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
 
 closeBtn.addEventListener("click", close_app);
 minimizeBtn.addEventListener("click", minimize_app);
 restoreBtn.addEventListener("click", restore_app);
 
-button1.addEventListener("click", next_page);
-button2.addEventListener("click", next_page2);
 
 function close_app() {
   app.window.close();
@@ -24,24 +20,7 @@ function restore_app() {
   app.window.restore();
 }
 
-function fade_out() {
-  const body = document.getElementsByTagName("body")[0];
-  body.style.opacity = "0";
-}
 
-function next_page() {
-  fade_out();
-  setTimeout(() => {
-    app.window.pass();
-  }, 500);
-}
-
-function next_page2() {
-  fade_out();
-  setTimeout(() => {
-    app.window.pass2();
-  }, 500);
-}
 
 document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("loaded");
