ce6299ce3d8f67d3344e0d78606430d28c64c14c
MigueldeHaroce <migueldeharoce@gmail.com>
2023-04-11 13:49:00 +0000

    Apr 11, 2023, 3:49 PM

--- AirosFiles/src/appFunctions.js
+++ AirosFiles/src/appFunctions.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function performSearch() {
     const query = textInput.value;
     if (query) {
-      window.location.href = 'searchPage.html?q=${encodeURIComponent(query)}';
+      window.location.href = `searchPage.html?q=${encodeURIComponent(query)}`;
     }
   }
   // Check if the searchResults WebView element exists
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
     const query = urlParams.get('q');
     if (query) {
-      searchResults.src = 'https://www.google.com/';
+      searchResults.src = `https://www.google.com/search?q=${query}`;
     } else {
       // Redirect back to the search menu if no query is provided
       searchResults.src = 'searchMenu.html';
