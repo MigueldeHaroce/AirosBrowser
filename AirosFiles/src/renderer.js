4fcd33f52a6b6d805bc0cef92c2f8d5ee5231b51
MigueldeHaroce <migueldeharoce@gmail.com>
2023-04-14 13:32:59 +0000

    Apr 14, 2023, 3:32 PM

--- AirosFiles/src/renderer.js
+++ AirosFiles/src/renderer.js
@@ -4,20 +4,18 @@ const searchBtn = document.getElementById('searchBtn');
 const textInput = document.getElementById('text-input');
 
 textInput.addEventListener('keydown', (event) => {
-if (event.key === 'Enter') {
-    console.log("a")
+  if (event.key === 'Enter') {
     search();
-}
+  }
 });
 
 searchBtn.addEventListener('click', () => {
-    console.log("a")
-    search();
+  search();
 });
 
 function search() {
-    const searchText = textInput.value;
-    if (searchText) {
-        ipcRenderer.invoke('search-text', searchText);
-    }
+  const searchText = textInput.value;
+  if (searchText) {
+    ipcRenderer.invoke('search-text', searchText);
+  }
 }
