cd8d32f972e1c40321e3099321b5b6be3e774b31
MigueldeHaroce <migueldeharoce@gmail.com>
2023-04-14 13:12:38 +0000

    Apr 14, 2023, 3:12 PM

--- AirosFiles/src/appFunctions.js
+++ AirosFiles/src/appFunctions.js
@@ -28,27 +28,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // get the input field and search button elements
 // get the input field and search button elements
-const searchInput = document.getElementById("text-input");
-const searchBtn = document.getElementById("searchBtn");
 
-// add event listeners for the enter key and search button click
-searchInput.addEventListener("keydown", function (event) {
-  if (event.code === "Enter") {
-    search();
-  }
-});
 
-searchBtn.addEventListener("click", function (event) {
-  search();
-});
 
-// define the search function
-function search() {
-  const query = searchInput.value;
-  const searchUrl = `https://www.google.com/search?q=${query}`;
-
-  // navigate to the search page and pass the search URL as a parameter
-  window.location.href = `searchPage.html?searchUrl=${encodeURIComponent(
-    searchUrl
-  )}`;
-}
