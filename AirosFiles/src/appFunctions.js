272455c462391b404bce1f3859ce232b772f75ce
MigueldeHaroce <migueldeharoce@gmail.com>
2023-04-22 15:37:38 +0000

    Apr 22, 2023, 5:37 PM

--- AirosFiles/src/appFunctions.js
+++ AirosFiles/src/appFunctions.js
@@ -25,33 +25,37 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-const searchBtn = document.getElementById('searchBtn');
-const textInput = document.getElementById('text-input');
+document.addEventListener('DOMContentLoaded', () => {
+  const searchBtn = document.getElementById('searchBtn');
+  const textInput = document.getElementById('text-input');
 
-textInput.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
+  textInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      search();
+    }
+  });
+
+  searchBtn.addEventListener('click', () => {
     search();
+  });
+
+  function search() {
+    const searchText = textInput.value;
+    const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchText)}`;
+
+    const webview = document.getElementById('searchResults')
+    if (webview) {
+      webview.addEventListener('did-finish-load', () => {
+        // Modify the URL of the webview here
+        webview.src(searchUrl)
+      })
+    } else {
+      console.error("not found webview")
+    }
   }
 });
 
-searchBtn.addEventListener('click', () => {
-  search();
-});
 
-function search() {
-  const searchText = textInput.value;
-  const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchText)}`;
-
-  const webview = document.getElementById('searchResults')
-  if (webview) {
-    webview.addEventListener('did-finish-load', () => {
-      // Modify the URL of the webview here
-      webview.src(searchUrl)
-    })
-  } else {
-    console.error("not found webview");
-  }
-}
 // get the input field and search button elements
 // get the input field and search button elements
 
