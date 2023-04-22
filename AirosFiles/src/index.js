9d0556f606712f9f823d551f053c5ed99a577f48
MigueldeHaroce <migueldeharoce@gmail.com>
2023-04-22 19:39:11 +0000

    Apr 22, 2023, 9:39 PM

--- AirosFiles/src/index.js
+++ AirosFiles/src/index.js
@@ -85,14 +85,9 @@ ipcMain.on("pass_page2", () => {
 ipcMain.on('search', (event, query) => {
   const url = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
   
-  let win = BrowserWindow.getFocusedWindow();
-  win.loadURL('file://' + __dirname + '/searchPage.html');
+  BrowserWindow.getFocusedWindow().loadURL('file://' + __dirname + '/searchPage.html');
   
-  
-  win.webContents.on('did-finish-load', () => {
-    win.webContents.send('search-results', url);
-  });
-  
+  event.sender.send('search-results', url);
   console.error('Received search event with data:', query);
   console.error('Constructed URL:', url);
 });
