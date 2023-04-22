5fe9dc43f580abee4944f0e83da92322c5f8d391
MigueldeHaroce <migueldeharoce@gmail.com>
2023-04-22 18:13:09 +0000

    Apr 22, 2023, 8:13 PM

--- AirosFiles/src/search.js
+++ AirosFiles/src/search.js
@@ -1,8 +1,8 @@
 const searchResults = document.querySelector('#searchResults');
-console.log('searchResults:', searchResults);
+console.error('searchResults:', searchResults);
 
 ipcRenderer.on('search-results', (event, results) => {
-  console.log('Received search-results event with data:', results);
+  console.error('Received search-results event with data:', results);
   if (searchResults && results) {
     searchResults.src = results;
   } else {
