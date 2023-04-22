67ca5116c340e629de1cd6a56fcde75c2464cb69
MigueldeHaroce <migueldeharoce@gmail.com>
2023-04-22 20:52:24 +0000

    Apr 22, 2023, 10:52 PM

--- AirosFiles/src/search.js
+++ AirosFiles/src/search.js
@@ -9,3 +9,21 @@ ipcRenderer.on('search-results', (event, results) => {
     console.error('Error: searchResults or results is undefined.');
   }
 });
+
+// Get searchID from URL
+const searchID = new URLSearchParams(window.location.search).get("searchID");
+
+// Pull search results using searchID
+if (searchID) {
+  ipcRenderer.send('pull-search', searchID);
+}
+
+// Set up listener for search-results event
+ipcRenderer.on('search-results', (event, results) => {
+  console.log('Received search-results event with data:', results);
+  if (searchResults && results) {
+    searchResults.src = results;
+  } else {
+    console.error('Error: searchResults or results is undefined.');
+  }
+});
