4c3d56fe25a10ae1789c60815eab9c82299401e2
MigueldeHaroce <migueldeharoce@gmail.com>
2023-04-23 10:41:32 +0000

    Apr 23, 2023, 12:41 PM

--- AirosFiles/src/preload.js
+++ AirosFiles/src/preload.js
@@ -23,8 +23,3 @@ contextBridge.exposeInMainWorld('ipcRenderer', {
     send: (channel, data) => ipcRenderer.send(channel, data),
     on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
 });
-
-contextBridge.exposeInMainWorld('ipcMain', {
-    send: (channel, data) => ipcMain.send(channel, data),
-    on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
-});
