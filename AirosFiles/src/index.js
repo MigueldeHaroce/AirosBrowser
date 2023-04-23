5a4dcf2e9d9555e57a2dc9bf089a6a15d4e5e2b6
MigueldeHaroce <migueldeharoce@gmail.com>
2023-04-23 10:17:51 +0000

    Apr 23, 2023, 12:17 PM

--- AirosFiles/src/index.js
+++ AirosFiles/src/index.js
@@ -83,22 +83,22 @@ const resultsStack = { /* ID: results */ };
 
 ipcMain.on('search', (event, query) => {
   const url = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
-  console.error(query);
-  console.error(url);
+  console.log(query);
+  console.log(url);
   const searchID = "ID-" + new Date().getTime();
   resultsStack[searchID] = url;
   BrowserWindow.getFocusedWindow().loadURL('file://' + __dirname + '/searchPage.html?searchID='+ searchID);
 });
 
 ipcMain.on('pull-search', (event, searchID) => {
-  console.error(searchID);
-  console.error(resultsStack[ searchID ]);
+  console.log(searchID);
+  console.log(resultsStack[ searchID ]);
 
   if (resultsStack[ searchID ]) {
     event.sender.send('search-results', resultsStack[ searchID ]);
     delete resultsStack[ searchID ];
   } else {
-    console.error("no id");
+    console.log("no id");
     event.sender.send('search-results', { error: "Unknown ID" });
   }
 });
