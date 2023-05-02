0d28e2f98f37b36bb7c251d6a26832587a3bb778
MigueldeHaroce <migueldeharoce@gmail.com>
2023-05-02 18:58:35 +0000

    May 2, 2023, 8:58 PM

---
+++ AirosFiles/src/tabs.js
@@ -0,0 +1,36 @@
+const addBtn = document.querySelector('.tabs');
+const tabWrapper = document.querySelector('.openBtns');
+
+addBtn.addEventListener('click', () => {
+  // Create a new tab element
+  const newTab = document.createElement('div');
+  newTab.classList.add('tabs');
+  newTab.innerHTML = `
+    <img id="logoTab" src="icons/logo18pxMono.png"></img>
+    <div id="textTab">
+        <span id="text">New Tab</span>
+    </div>
+    <div id="cross-button">
+        <img id="imgCross" src="icons/cross.png">
+    </div> 
+  `;
+
+  // Insert the new tab after the last tab
+  const tabs = document.querySelectorAll('.tabs');
+  tabWrapper.insertBefore(newTab, tabs[tabs.length - 1].nextSibling);
+
+  // Animate the new tab
+  const tabWidth = newTab.offsetWidth;
+  newTab.style.width = '0px';
+  newTab.style.opacity = '0';
+  newTab.style.display = 'flex';
+  setTimeout(() => {
+    newTab.style.width = `${tabWidth}px`;
+    newTab.style.opacity = '1';
+  }, 0);
+
+  // Move the add button after the new tab
+  const addButtonWidth = addBtn.offsetWidth;
+  const addButtonLeft = addBtn.getBoundingClientRect().left;
+  addBtn.style.left = `${addButtonLeft + tabWidth + addButtonWidth}px`;
+});
