1ea2c5332912f000b66984be403d6faa72bd0f7d
MigueldeHaroce <migueldeharoce@gmail.com>
2023-05-02 19:24:59 +0000

    May 2, 2023, 9:24 PM

--- AirosFiles/src/tabs.js
+++ AirosFiles/src/tabs.js
@@ -6,38 +6,39 @@ if (addBtn) {
 }
 
 addBtn.addEventListener('click', () => {
-    // Create a new tab element
-    const newTab = document.createElement('div');
-    newTab.classList.add('tabs');
-    newTab.innerHTML = `
-      <img id="logoTab" src="icons/logo18pxMono.png"></img>
-      <div id="textTab">
-          <span id="text">New Tab</span>
-      </div>
-      <div id="cross-button">
-          <img id="imgCross" src="icons/cross.png">
-      </div> 
-    `;
-  
-    // Insert the new tab after the last tab
-    const tabs = document.querySelectorAll('.tabs');
+  // Create a new tab element
+  const newTab = document.createElement('div');
+  newTab.classList.add('tabs');
+  newTab.innerHTML = `
+    <img id="logoTab" src="icons/logo18pxMono.png"></img>
+    <div id="textTab">
+      <span id="text">New Tab</span>
+    </div>
+    <div id="cross-button">
+      <img id="imgCross" src="icons/cross.png">
+    </div> 
+  `;
+
+  // Insert the new tab after the last tab
+  const tabs = document.querySelectorAll('.tabs');
+  if (tabs.length > 0) {
     tabWrapper.insertBefore(newTab, tabs[tabs.length - 1].nextSibling);
-  
-    // Animate the new tab
-    const tabWidth = newTab.offsetWidth;
-    newTab.style.width = '0px';
-    newTab.style.opacity = '0';
-    newTab.style.display = 'flex';
-    setTimeout(() => {
-      newTab.style.width = `${tabWidth}px`;
-      newTab.style.opacity = '1';
-  
-      // Move the add button after the new tab
-      const addButtonWidth = addBtn.offsetWidth;
-      const lastTab = tabs[tabs.length - 2];
-      const lastTabRight = lastTab.getBoundingClientRect().right;
-      const addButtonLeft = lastTabRight + addButtonWidth;
-      addBtn.style.left = `${addButtonLeft}px`;
-    }, 0);
-  });
-  
+  } else {
+    tabWrapper.appendChild(newTab);
+  }
+
+  // Animate the new tab
+  const tabWidth = newTab.offsetWidth;
+  newTab.style.width = '0px';
+  newTab.style.opacity = '0';
+  newTab.style.display = 'flex';
+  setTimeout(() => {
+    newTab.style.width = `${tabWidth}px`;
+    newTab.style.opacity = '1';
+  }, 0);
+
+  // Move the add button after the new tab
+  const addButtonWidth = addBtn.offsetWidth;
+  const addButtonLeft = addBtn.getBoundingClientRect().left;
+  addBtn.style.left = `${addButtonLeft + tabWidth + addButtonWidth}px`;
+});
