91b7e06bfd4f63c73965199bf9a612a244725872
MigueldeHaroce <migueldeharoce@gmail.com>
2023-05-04 16:09:02 +0000

    May 4, 2023, 6:09 PM

--- AirosFiles/src/tabs.js
+++ AirosFiles/src/tabs.js
@@ -1,13 +1,14 @@
 const tabWrapper = document.querySelector('.divTabs');
 const addBtn = document.querySelector('.openBtns');
-const closeBtn = document.querySelector('#cross-button');
+const closeBtns = document.querySelectorAll('#cross-button');
 
 let numTabs = 0;
 
 if (addBtn) {
   addBtn.addEventListener('click', () => {
-
+    // Increment the number of tabs
     numTabs++;
+
     // Create a new tab element
     const newTab = document.createElement('div');
     newTab.classList.add('tabs');
@@ -16,15 +17,15 @@ if (addBtn) {
       <div id="textTab">
           <span id="text">New Tab</span>
       </div>
-      <div id="cross-button">
-          <img id="imgCross" src="icons/cross.png">
+      <div class="cross-button">
+          <img class="imgCross" src="icons/cross.png">
       </div> 
     `;
 
     // Insert the new tab after the last tab
     const tabs = document.querySelectorAll('.tabs');
     tabWrapper.insertBefore(newTab, tabs[tabs.length - 1].nextSibling);
-  
+
     // Animate the new tab
     const tabWidth = newTab.offsetWidth;
     newTab.style.width = '0px';
@@ -33,7 +34,7 @@ if (addBtn) {
     setTimeout(() => {
       newTab.style.width = `${tabWidth}px`;
       newTab.style.opacity = '1';
-  
+
       // Move the add button after the new tab
       const addButtonWidth = addBtn.offsetWidth;
       const lastTab = tabs[tabs.length - 2];
@@ -44,29 +45,31 @@ if (addBtn) {
   });
 }
 
-if (closeBtn) {
-  closeBtn.addEventListener('click', () => {
-    // Get the tab that contains the close button
-    const tab = closeBtn.parentNode;
+if (closeBtns) {
+  closeBtns.forEach((closeBtn) => {
+    closeBtn.addEventListener('click', () => {
+      // Get the tab that contains the close button
+      const tab = closeBtn.parentNode.parentNode;
 
-    // If there's only one tab open, close the app
-    if (numTabs <= 1) {
-      window.close();
-    } else {
-      // Otherwise, remove the tab
-      const tabWidth = tab.offsetWidth;
-      tab.style.width = `${tabWidth}px`;
-      tab.style.opacity = '0';
-      setTimeout(() => {
-        tabWrapper.removeChild(tab);
+      // If there's only one tab open, close the app
+      if (numTabs <= 1) {
+        window.close();
+      } else {
+        // Otherwise, remove the tab
+        const tabWidth = tab.offsetWidth;
+        tab.style.width = `${tabWidth}px`;
+        tab.style.opacity = '0';
+        setTimeout(() => {
+          tabWrapper.removeChild(tab);
 
-        // Move the add button back to its original position
-        const addButtonLeft = addBtn.getBoundingClientRect().left;
-        addBtn.style.left = `${addButtonLeft - tabWidth}px`;
+          // Move the add button back to its original position
+          const addButtonLeft = addBtn.getBoundingClientRect().left;
+          addBtn.style.left = `${addButtonLeft - tabWidth}px`;
 
-        // Update the number of tabs
-        numTabs--;
-      }, 300);
-    }
+          // Update the number of tabs
+          numTabs--;
+        }, 300);
+      }
+    });
   });
 }
