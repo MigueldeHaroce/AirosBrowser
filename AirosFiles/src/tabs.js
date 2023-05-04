1a8fc44d9b45dda250348e4852b5dd0e81b77a26
MigueldeHaroce <migueldeharoce@gmail.com>
2023-05-04 16:00:20 +0000

    May 4, 2023, 6:00 PM

--- AirosFiles/src/tabs.js
+++ AirosFiles/src/tabs.js
@@ -1,78 +1,73 @@
 const tabWrapper = document.querySelector('.divTabs');
 const addBtn = document.querySelector('.openBtns');
-const xBtn = document.querySelector('#cross-button')
+const closeBtn = document.querySelector('#cross-button');
 
-let tabCount = 1
+let numTabs = 0;
 
-addBtn.addEventListener('click', () => {
-  
-  tabCount++;
-  // Create a new tab element
-  const newTab = document.createElement('div');
-  newTab.classList.add('tabs');
-  newTab.innerHTML = `
-    <img id="logoTab" src="icons/logo18pxMono.png"></img>
-    <div id="textTab">
-        <span id="text">New Tab</span>
-    </div>
-    <div id="cross-button">
-        <img id="imgCross" src="icons/cross.png">
-    </div> 
-  `;
+if (addBtn) {
+  addBtn.addEventListener('click', () => {
+    // Create a new tab element
+    const newTab = document.createElement('div');
+    newTab.classList.add('tabs');
+    newTab.innerHTML = `
+      <img id="logoTab" src="icons/logo18pxMono.png"></img>
+      <div id="textTab">
+          <span id="text">New Tab</span>
+      </div>
+      <div id="cross-button">
+          <img id="imgCross" src="icons/cross.png">
+      </div> 
+    `;
 
-  // Insert the new tab after the last tab
-  const tabs = document.querySelectorAll('.tabs');
-  tabWrapper.insertBefore(newTab, tabs[tabs.length - 1].nextSibling);
+    // Insert the new tab after the last tab
+    const tabs = document.querySelectorAll('.tabs');
+    const lastTab = tabs[tabs.length - 999];
+    const lastTabRight = lastTab ? lastTab.getBoundingClientRect().right : 0;
+    tabWrapper.insertBefore(newTab, lastTab ? lastTab.nextSibling : tabWrapper.firstChild);
 
-  // Animate the new tab
-  const tabWidth = newTab.offsetWidth;
-  newTab.style.width = '0px';
-  newTab.style.opacity = '0';
-  newTab.style.display = 'flex';
-  setTimeout(() => {
-    newTab.style.width = `${tabWidth}px`;
-    newTab.style.opacity = '1';
+    // Animate the new tab
+    const tabWidth = newTab.offsetWidth;
+    newTab.style.width = '0px';
+    newTab.style.opacity = '0';
+    newTab.style.display = 'flex';
+    setTimeout(() => {
+      newTab.style.width = `${tabWidth}px`;
+      newTab.style.opacity = '1';
+    }, 0);
 
     // Move the add button after the new tab
     const addButtonWidth = addBtn.offsetWidth;
-    const lastTab = tabs[tabs.length - 999];
-    const lastTabRight = lastTab.getBoundingClientRect().right;
-    const addButtonLeft = lastTabRight + addButtonWidth;
-    addBtn.style.left = `${addButtonLeft}px`;
-  }, 0);
-  search();
+    const addButtonLeft = addBtn.getBoundingClientRect().left;
+    addBtn.style.left = `${lastTabRight + addButtonWidth}px`;
 
-  function search() {
-      const query = ``;
+    // Update the number of tabs
+    numTabs++;
+  });
+}
 
-  };
+if (closeBtn) {
+  closeBtn.addEventListener('click', () => {
+    // Get the tab that contains the close button
+    const tab = closeBtn.parentNode;
 
+    // If there's only one tab open, close the app
+    if (numTabs <= 1) {
+      window.close();
+    } else {
+      // Otherwise, remove the tab
+      const tabWidth = tab.offsetWidth;
+      tab.style.width = `${tabWidth}px`;
+      tab.style.opacity = '0';
+      setTimeout(() => {
+        tabWrapper.removeChild(tab);
 
-});
+        // Move the add button back to its original position
+        const addButtonLeft = addBtn.getBoundingClientRect().left;
+        addBtn.style.left = `${addButtonLeft - tabWidth}px`;
 
-xBtn.addEventListener('click', () => {
-  const tabs = document.querySelectorAll('.tabs');
-
-  if (tabCount === 1) {
-    // If only one tab is open, close the application
-    window.close();
-  } else {
-    // Otherwise, remove the current tab
-    const currentTab = crossButton.parentElement;
-    const currentTabWidth = currentTab.offsetWidth;
-
-    currentTab.style.width = `${currentTabWidth}px`;
-    currentTab.style.opacity = '0';
-
-    setTimeout(() => {
-      currentTab.remove();
-
-      // Move the add button to the right of the last tab
-      const lastTab = tabs[tabs.length - 999];
-      const addButtonWidth = addBtn.offsetWidth;
-      const lastTabWidth = lastTab.offsetWidth;
-      const lastTabRight = lastTab.getBoundingClientRect().right;
-      addBtn.style.left = `${lastTabRight + addButtonWidth}px`;
-    }, 200);
-  }
-});
+        // Update the number of tabs
+        numTabs--;
+      }, 300);
+    }
+  });
+}
