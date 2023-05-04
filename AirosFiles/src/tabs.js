35a810758fe5c1921609b9b274d6aff7f3f2eae9
MigueldeHaroce <migueldeharoce@gmail.com>
2023-05-04 17:45:15 +0000

    May 4, 2023, 7:45 PM

--- AirosFiles/src/tabs.js
+++ AirosFiles/src/tabs.js
@@ -5,6 +5,7 @@ let numTabs = 2;
 if (addBtn) {
   addBtn.addEventListener('click', () => {
     numTabs++;
+
     // Create a new tab element
     const newTab = document.createElement('div');
     newTab.classList.add('tabs');
@@ -16,8 +17,15 @@ if (addBtn) {
       <div class="cross-button">
           <img id="imgCross" class="imgCross" src="icons/cross.png">
       </div> 
-      <webview id="webview${numTabs}" class="searchResults"></webview>
     `;
+
+    // Create a new webview element
+    const newWebView = document.createElement('webview');
+    newWebView.setAttribute('id', `searchResults${numTabs}`);
+    newWebView.setAttribute('src', 'https://www.google.com');
+
+    // Append the new webview element to the tab element
+    newTab.appendChild(newWebView);
 
     // Insert the new tab after the last tab
     const tabs = document.querySelectorAll('.tabs');
@@ -42,22 +50,31 @@ if (addBtn) {
   });
 }
 
-let currentTab = document.querySelector('#webview1');
-
 tabWrapper.addEventListener('click', (event) => {
   const target = event.target;
   if (target && target.classList.contains('imgCross')) {
     const tab = target.closest('.tabs');
     const tabWidth = tab.offsetWidth;
     tab.style.width = `${tabWidth}px`;
+    tab.style.opacity = '0';
+
+    // Remove the associated webview element
+    const webviewId = `searchResults${numTabs}`;
+    const webview = document.getElementById(webviewId);
+    if (webview) {
+      webview.remove();
+    }
+
     setTimeout(() => {
       tab.remove();
-    }, 300);
-  } else if (target && target.classList.contains('tabs')) {
-    const tab = target;
-    const tabId = tab.querySelector('webview').id;
-    currentTab.style.display = 'none';
-    currentTab = document.querySelector(`#${tabId}`);
-    currentTab.style.display = 'flex';
+
+      // Move the add button after the last tab
+      const addButtonWidth = addBtn.offsetWidth;
+      const tabs = document.querySelectorAll('.tabs');
+      const lastTab = tabs[tabs.length - 2];
+      const lastTabRight = lastTab.getBoundingClientRect().right;
+      const addButtonLeft = lastTabRight + addButtonWidth;
+      addBtn.style.left = `${addButtonLeft}px`;
+    }, 250);
   }
 });
