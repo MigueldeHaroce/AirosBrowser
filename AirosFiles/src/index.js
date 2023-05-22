6596fd44def705e47d2e318138ae9b0676cb320c
MigueldeHaroce <migueldeharoce@gmail.com>
2023-05-22 15:50:35 +0000

    May 22, 2023, 5:50 PM

--- AirosFiles/src/index.js
+++ AirosFiles/src/index.js
@@ -36,6 +36,7 @@ const createWindow = () => {
   // Open the DevTools
 };
 
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
